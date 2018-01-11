24c158682cb7e2c8069bad9d0e295a0d486ccb9b
kisumutalent <homiemusa@gmail.com>
2018-01-11 10:28:23 +0300

    creating forms for devise in org_people and mails activations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,24 @@ Rails.application.configure do
 
 
 #devise section
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+config.action_mailer.default_options = {from: 'no-reply@projectmeal.com'}
+config.action_mailer.default_url_options = { host: 'localhost:3000'}
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.default :chartset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  part: 587,
+  domain: 'localhost:3000',
+  user_name: 'homiemusa@gmail.com',
+  password: 'djrefuge',
+  authentication: 'plain',
+  enable_starttls_auto: true
+
+}
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
